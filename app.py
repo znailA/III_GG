e6f2caaf12fe779c8899f2c8d607e93819c23732
znailA <aliadnan807558@gmail.com>
2026-04-14 18:05:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Hello from Docker!"
 
-app.run(host="0.0.0.0", port=5000)
+port = int(os.environ.get("PORT", 8080))
+
+app.run(host="0.0.0.0", port=port)
