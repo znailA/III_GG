6a0cb86f565763f1d399311d58f59bebb7ae34aa
znailA <aliadnan807558@gmail.com>
2026-04-14 18:26:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Docker!"
+    return "Hello from Docker! Telegram:@III_GG"
 
-port = int(os.environ.get("PORT", 8080))
-
-app.run(host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
